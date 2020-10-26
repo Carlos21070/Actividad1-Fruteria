50b0ac49d6eee7d4390d8180b6e5ad525f7582a3
Carlos Alejandro Ramos Hernández <carlos.ramos99@hotmail.com>
2020-10-26 14:07:00 -0600

    version1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Actividad1_FruitStore.Models;
+using Actividad1_FruitStore.Models.ViewModels;
+using Actividad1_FruitStore.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad1_FruitStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("Home/Index")]
+        [Route("Home")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(); 
+        }
+        
+        [Route("{id}")]
+        public IActionResult Categoria(string id)
+        {
+            using (fruteriashopContext context = new fruteriashopContext())
+            {
+
+                ProductosRepository repos = new ProductosRepository(context);
+                CategoriaViewModel vm = new CategoriaViewModel();
+                vm.Nombre= id;
+                vm.Productos = repos.GetProductosByCategoria(id).ToList();
+
+                return View(vm);
+            }
+        }
+
+        [Route("detalles/{categoria}/{id}")]
+        public IActionResult Ver(string categoria, string id)
+        {
+            using (fruteriashopContext context = new fruteriashopContext())
+            {
+                ProductosRepository repos = new ProductosRepository(context);
+
+                Productos p = repos.GetProductoByCategoriaNombre(categoria, id.Replace("-", " "));
+
+                return View(p);
+            }
+        }
+    }
+}
---
+++ Services/CategoriasService.cs
@@ -0,0 +1,23 @@
+﻿using Actividad1_FruitStore.Models;
+using Actividad1_FruitStore.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Actividad1_FruitStore.Services
+{
+    public class CategoriasService
+    {
+        public List<Categorias> Categorias { get; set; }
+
+        public CategoriasService()
+        {
+            using (fruteriashopContext context = new fruteriashopContext())
+            {
+                Repository<Categorias> repos = new Repository<Categorias>(context);
+                Categorias = repos.GetAll().Where(x=>x.Eliminado==0).OrderBy(x=>x.Nombre).ToList();
+            }
+        }
+    }
+}
